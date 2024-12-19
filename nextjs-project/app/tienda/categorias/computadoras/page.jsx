f5ed95523885be25dc3cc0a915341f010f6514a3
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2024-12-18 23:27:57 -0400

    Nextjs Router

---
+++ nextjs-project/app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,5 @@
+export default function Computadoras() {
+  return (
+    <div>Computadoras</div>
+  );
+}
---
+++ nextjs-project/app/tienda/page.jsx
@@ -0,0 +1,10 @@
+export default function Tienda() {
+  return (
+    <div>
+      Tienda
+      <ul>
+        <li>Product 1</li>
+      </ul>
+    </div>
+  );
+}
